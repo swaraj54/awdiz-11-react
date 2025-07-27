c2b36606cff3997f3463733a4354a8fc291ac922
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-07-27 14:02:08 +0530

    day-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <h1>Helo Everyone skdene</h1>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
+
+// 1. Real dom - size is bigger / heavy, in ui
+
+// 1. virtual dom - lightweigh copy of real dom, in memory
+// <div></div>
+
+// 2. virtual dom - fastrer chanfge
+// <div>Hello world</div>
+
+// compare 1 and 2 virtual dom
+// <div>Hello world</div>
+
+// Real dom
+// <div>Hello world</div>
