71af5f60c85e2b4e61478b4e763ed92a4df1fbfd
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2025-08-20 20:59:02 +0530

    Updatses

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import UseMemo from "./pages/10-08/UseMemo";
 import UseCallback from "./pages/10-08/UseCallback";
 import UseRef from "./pages/13-08/UseRef";
 import MultiStepperForm from "./pages/17-08/MultiStepperForm";
+import UseReducer from "./pages/20-08/UseReducer";
 
 function App() {
   const [users, setUsers] = useState(["Virat", "Rohit", "Dhoni"]);
@@ -52,6 +53,7 @@ function App() {
         <Route path='/use-callback' element={<UseCallback />} />
         <Route path='/use-ref' element={<UseRef />} />
         <Route path='/multi-stepper-form' element={<MultiStepperForm />} />
+        <Route path='/use-reducer' element={<UseReducer />} />
       </Routes>
     </div>
   );
@@ -76,7 +78,7 @@ export default App;
 
 // useReducer 
 // useContext 
-
+c
 
 // redux 
 
---
+++ src/pages/20-08/UseReducer.jsx
@@ -0,0 +1,35 @@
+import React, { act, useReducer, useState } from "react";
+
+const UseReducer = () => {
+  function reducer(state, action) {
+    console.log("Inside Reducer", state, "state", action, "action");
+    // if (action.type === "increment") {
+    //   return { count: state.count + 1 };
+    // } else if (action.type === "decrement") {
+    //   return { count: state.count - 1 };
+    // }
+    switch (action.type) {
+      case "increment":
+        return { ...state, count: state.count + 1 };
+      case "decrement":
+        return { ...state, count: state.count - 1 };
+      case "reset":
+        return { ...state, count: 0 };
+      default:
+        return state;
+    }
+  }
+  const initialState = { count: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state, "state");
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+    </div>
+  );
+};
+
+export default UseReducer;
